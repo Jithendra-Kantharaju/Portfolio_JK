e71df6c7e96ec278d7641b7684092927de76135a
jk <jithendragowda0203@gmail.com>
2025-08-08 20:44:51 -0700

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // ADD THIS LINE FOR GITHUB PAGES
+  base: '/Portfolio_JK/', 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
